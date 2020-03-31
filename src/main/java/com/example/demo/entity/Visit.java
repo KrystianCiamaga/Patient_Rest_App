2a6130c43423e715383d4d8f231482a9a04b90c7
Krystian Ciamaga <krystianciamaga@gmail.com>
2020-03-31 22:25:52 +0200

    Added visti entity

--- src/main/java/com/example/demo/entity/Visit.java
+++ src/main/java/com/example/demo/entity/Visit.java
@@ -1,4 +1,35 @@
 package com.example.demo.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Visit {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate visitDate;
+
+
+    @ManyToOne
+    private Patient patient;
+
+
+
 }
