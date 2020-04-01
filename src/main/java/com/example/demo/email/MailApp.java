eb073668c0f9d28af71d65d8091d1b2e3019c5fe
Krystian Ciamaga <krystianciamaga@gmail.com>
2020-04-01 21:00:14 +0200

    Added mail class

---
+++ src/main/java/com/example/demo/email/MailApp.java
@@ -0,0 +1,26 @@
+package com.example.demo.email;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+
+@RestController
+public class MailApp {
+
+    private MailService mailService;
+
+    public MailApp(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/sendmail")
+    public String sendMail() throws MessagingException{
+
+        mailService.sendMail("dupa@gmail.com",   "Wygrałeś",
+                "<b>1000 000 zł</b><br>:P", true);
+
+        return "wwygrales";
+
+    }
+}
